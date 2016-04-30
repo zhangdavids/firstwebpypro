d4f7810500a7560fbf4ff0793b2787fafc47edc5
zhangdavids <1269969739@qq.com>
2016-04-30 16:34:48 +0800

    second commit

--- Documents/Aptana Studio 3 Workspace/mywebpy/code.py
+++ Documents/Aptana Studio 3 Workspace/mywebpy/code.py
@@ -3,9 +3,23 @@ import web
 urls = (
        '/','index')
 
+movies = [
+    {
+        'title': 'Forrest Gump',
+        'year': 1994,
+    },
+    {
+        'title': 'Titanic',
+        'year': 1997,
+    }
+]
+
 class index:
     def GET(self):
-        return "Hello,world!"
+        page = ''
+        for m in movies:
+            page += '%s (%d)\n' % (m['title'], m['year'])
+        return page
     
 if __name__=="__main__":
     app = web.application(urls,globals())
